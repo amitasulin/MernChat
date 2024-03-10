c06b8066f209b7f27035ea0c0f5a973e425d5592
amitasulin <amitasulin7@gmail.com>
2024-03-10 19:08:19 +0200

    Initial commit

---
+++ mern-chat-backend/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.ridemiu.mongodb.net/?retryWrites=true&w=majority`,
+
+  () => {
+    console.log("connected to mongodb");
+  }
+);
